50dcc08c5ba6da52e7f98005e23e19cb095a5419
Maciej Kostuch <mkostuch@tdwizard.com>
2014-10-13 15:17:22 +0200

    remove unnecessary code from model

--- cmsplugin_bootstrap_carousel/models_default.py
+++ cmsplugin_bootstrap_carousel/models_default.py
@@ -1,17 +1,11 @@
 # coding: utf-8
-import os
 from django.db import models
-from django.conf import settings
-from django.core.files.uploadedfile import SimpleUploadedFile
 from django.utils.translation import ugettext as _
 
 from cms.models.pluginmodel import CMSPlugin
-from PIL import Image
-from cStringIO import StringIO
 from filebrowser.fields import FileBrowseField
 
-DEF_SIZE = (800, 600)
-        
+
 class Carousel(CMSPlugin):
     domid = models.CharField(max_length=50, verbose_name=_('Name'))
     interval = models.IntegerField(default=5000)
@@ -24,6 +18,7 @@ class Carousel(CMSPlugin):
     
     def __unicode__(self):
         return self.domid
+
 
 class CarouselItem(models.Model):
     carousel = models.ForeignKey(Carousel)
