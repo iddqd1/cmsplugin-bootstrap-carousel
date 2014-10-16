831d3eb4b208feffc292f04202ccc86f1748f3a0
Maciej Kostuch <mkostuch@tdwizard.com>
2014-10-16 16:03:54 +0200

    Add video text field - when code in video image doesn't show. Paste iframe from Youtube to video area

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
         ]
     },
 
-    version = "0.14",
+    version = "0.15",
     description = "Bootstrap carousel plugin for django-cms 2.2",
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
     author = "Antoine Nguyen",
